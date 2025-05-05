eab6c633bcb5f39af77037727ebb8c1d6b12cb64
ABOUALOU Jihan <miouni499@gmail.com>
2025-05-05 12:49:33 +0200

    deb

--- src/test/java/Modele/PlageHoraireTest.java
+++ src/test/java/Modele/PlageHoraireTest.java
@@ -19,24 +19,24 @@ class PlageHoraireTest {
     }
 
     @Test
-    void duree() {
-        PlageHoraire horaire1 = new  PlageHoraire(new Horaire(8,30), new Horaire(12,0));;
+    void duree() throws Exception {
+        PlageHoraire horaire1 = new PlageHoraire(new Horaire(8,30), new Horaire(12,0));;
         assertEquals(120, horaire1.duree());
 
         // Cas 2 : horaire de fin avant l'horaire de début, donc durée négative
-        PlageHoraire horaire2 = new  PlageHoraire(new Horaire(8,00), new Horaire(10,0));
+        PlageHoraire horaire2 = new PlageHoraire(new Horaire(8,00), new Horaire(10,0));
         assertEquals(-120, horaire2.duree());
 
         // Cas 3 : durée devrait être 0
-        PlageHoraire horaire3 = new  PlageHoraire(new Horaire(8,00), new Horaire(8,00));
+        PlageHoraire horaire3 = new PlageHoraire(new Horaire(8,00), new Horaire(8,00));
         assertEquals(0, horaire3.duree());
 
         // Cas 4 : plage horaire entre 14h30 et 17h45 (195 minutes)
-        _v1.PlageHoraire horaire4 = new PlageHoraire(new Horaire(14,30), new Horaire(17,45));
+        PlageHoraire horaire4 = new PlageHoraire(new Horaire(14,30), new Horaire(17,45));
         assertEquals(195, horaire4.duree());
     }
 
-    @Test
-    void compareTo() {
-    }
+    //@Test
+    //void compareTo() {
+    //}
 }
